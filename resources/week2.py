de55f301746a50d7db5714d45ba61e2097e05f3f
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-20 17:12:16 +0530

    times

--- resources/week2.py
+++ resources/week2.py
@@ -58,3 +58,19 @@ Theta() - Tightly bound:
 Theta notation describes both the upper and lower bounds of an algorithm's running time. Specifically, we use the notation  to describe the tight bound of an algorithm's running time. This means that the algorithm's running time will grow at the same rate as some constant multiple of  as the input size grows.
 """
 
+"""It's important to note that big O notation describes the upper bound of the growth rate of a function. Some algorithms may have a best-case or average-case time complexity that is much faster than their worst-case time complexity. However, the worst-case time complexity is typically what is used to describe the time complexity of an algorithm.
+
+We often use "big O" notation to describe the growth rate of functions. This notation allows us to compare the time complexity of algorithms and the behavior of functions as their input size grows to infinity.
+
+Here are some common functions and their growth rates, listed from slowest-growing to fastest-growing:
+
+ - constant time
+ - logarithmic time
+ - linear time
+ - linearithmic time
+ - quadratic time
+ - cubic time
+ - exponential time
+ - factorial time
+The growth rate of a function is important when analyzing the time complexity of an algorithm. For example, if an algorithm has a time complexity of , it means that its running time grows quadratically with the input size. If the input size doubles, the running time will increase by a factor of four.
+"""
