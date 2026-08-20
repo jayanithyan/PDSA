e16f6827cec3c0b77efda068eed940cea55d4a1d
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-20 17:10:40 +0530

    growth rate

--- resources/week2.py
+++ resources/week2.py
@@ -24,3 +24,37 @@ Average case
 Worst Case"""
 
 
+"""
+Growth rate of functions
+The number of operations for an algorithm is usually expressed as a function of the input.
+
+For Example:
+
+s = 0 #1
+for i in range(n): #n+1
+    for j in range(n): #n(n+1)
+        s = s + 1 #n^2
+print(s)#1
+Function for given code is :
+
+
+Ignore all the constant and coefficient just look at the highest order term in relation to . So  is proportional to 
+
+ 
+
+Notations to represent complexity
+The notation are mathematical notations that are commonly used to describe the time complexity of an algorithm or the upper and lower bounds of how an algorithm's running time grows as the input size grows.
+
+Big-Oh() - Upper bound:
+
+Big  notation describes the upper bound of an algorithm's running time. Specifically, we use the notation  to describe the maximum growth rate of an algorithm's running time. This means that the algorithm's running time will not grow faster than some constant multiple of  as the input size grows.
+
+Omega() - Lower bound:
+
+Omega notation, on the other hand, describes the lower bound of an algorithm's running time. Specifically, we use the notation  to describe the minimum growth rate of an algorithm's running time. This means that the algorithm's running time will not grow slower than some constant multiple of as the input size grows.
+
+Theta() - Tightly bound:
+
+Theta notation describes both the upper and lower bounds of an algorithm's running time. Specifically, we use the notation  to describe the tight bound of an algorithm's running time. This means that the algorithm's running time will grow at the same rate as some constant multiple of  as the input size grows.
+"""
+
