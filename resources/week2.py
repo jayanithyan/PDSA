90baf4ac3299a1e52c3f7ac88990c299f13c5db4
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-20 17:09:54 +0530

    complexity

---
+++ resources/week2.py
@@ -0,0 +1,26 @@
+"""Complexity
+The complexity of an algorithm is a function describing the efficiency of the algorithm in terms of the amount of input data. There are two main complexity measures of the efficiency of an algorithm:
+
+Space complexity
+
+The space complexity of an algorithm is the amount of memory it needs to run to completion.
+
+Generally, space needed by an algorithm is the sum of the following two components:
+
+Fixed part() - Size of code
+
+Variable Part() - Depend on input size, to store in memory
+
+Total Space 
+
+Time complexity
+
+The time complexity of an algorithm is the amount of computer time it needs to run to completion. Computer time represents the number of operations executed by the processor.
+
+Time complexity calculated in three types of cases:
+
+Best case
+Average case
+Worst Case"""
+
+
